73cc56df673862a46e9ed8b05831b1ba6b4cb67e
Sissi <sissi_westin@hotmail.com>
2019-06-11 23:30:36 +0200

    Created the groundworks for activities. Made the activityDetails page show when clicking a button.

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.brom.activitiesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
@@ -9,11 +22,36 @@ public class MainActivity extends AppCompatActivity {
     private int[] mountainHeights ={4478,4808,6190};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
+    private ArrayList<String> listData = new ArrayList<>(Arrays.asList(mountainNames));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.list_item_textview,
+                R.id.listItemTextView,
+                listData);
 
+        ListView myListView = findViewById(R.id.listViewDisplay);
+
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Toast.makeText(getApplicationContext(),
+                        "Mountain: " + mountainNames[position] +
+                                "\n" +
+                                "Area: " + mountainLocations[position] +
+                                "\n" +
+                                "Height: " + mountainHeights[position] + " Meters",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
         // 3. Create a new Layout file for the MountainDetailsActivity called
@@ -31,7 +69,17 @@ public class MainActivity extends AppCompatActivity {
         // 7. Display the MountainDetailsActivity with the data from the Intent created in step
         //    6
         // 8. From the MountainDetailsActivity you should have an option to "go back" using an
-        //    left arro button. This is done by letting the MainActivity be the parent activity to
+        //    left arrow button. This is done by letting the MainActivity be the parent activity to
         //    MountainDetailsActivity.
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view)
+    {
+        // Do something in response to button
+        Intent intent = new Intent(this, details.class);
+        //EditText editText = (EditText) findViewById(R.id.activityDetails);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
